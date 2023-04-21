f02fd65a43658fa2e19d5c774e71d7ca6d36c3ae
黃柏澄(風雨) <wartw@users.noreply.github.com>
2023-04-21 17:04:48 +0800

    Update Buffer.php

--- src/Ethereum/RLP/Buffer.php
+++ src/Ethereum/RLP/Buffer.php
@@ -1,84 +1,312 @@
 <?php
 
-namespace Binance\Types;
+/**
+ * This file is part of rlp package.
+ *
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ *
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
 
-use BitWasp\Buffertools\BufferInterface;
+namespace xtype\Ethereum\RLP;
 
-class Buffer extends \BitWasp\Buffertools\Buffer
+use InvalidArgumentException;
+use ArrayAccess;
+
+class Buffer implements ArrayAccess
 {
     /**
-     * @param string $byteString
-     * @param int|null $byteSize
-     * @throws \Exception
+     * data
+     *
+     * @var array
      */
-    public function __construct(string $byteString = '', ?int $byteSize = null)
+    protected $data = array();
+
+    /**
+     * encoding
+     *
+     * @var string
+     */
+    protected $encoding = '';
+
+    /**
+     * construct
+     *
+     * @param mixed $data
+     * @param string $encoding the data encoding
+     * @return void
+     */
+    public function __construct($data=[], string $encoding='utf8')
     {
-        parent::__construct($byteString, $byteSize);
+        $this->encoding = strtolower($encoding);
+
+        if ($data) {
+            $this->data = $this->decodeToData($data);
+        }
     }
 
     /**
-     * @param string $hexString
-     * @param int|null $byteSize
-     * @return Buffer
-     * @throws \Exception
+     * offsetSet
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
      */
-    public static function hex(string $hexString = '', int $byteSize = null): BufferInterface
+    public function offsetSet($offset, $value): void
     {
-        $len = strlen($hexString);
-        if ($len > 0 && !ctype_xdigit($hexString)) {
-            throw new \InvalidArgumentException('Buffer::hex: non-hex character passed');
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
         }
-
-        $binary = pack("H*", $len % 2 === 0 ? $hexString : str_pad($hexString, $len + 1, '0', STR_PAD_LEFT));
-        return new static($binary, $byteSize);
     }
 
     /**
-     * @param int|string $integer
-     * @param null|int $byteSize
-     * @return Buffer
+     * offsetExists
+     *
+     * @param mixed $offset
+     * @return bool
      */
-    public static function int($integer, $byteSize = null): BufferInterface
+    public function offsetExists($offset): bool
     {
-        if ($integer < 0) {
-            throw new \InvalidArgumentException('Negative integers not supported by Buffer::int. This could be an application error, or you should be using templates.');
-        }
-
-        $hex = gmp_strval(gmp_init($integer, 10), 16);
-        if ((mb_strlen($hex) % 2) !== 0) {
-            $hex = "0{$hex}";
-        }
-
-        $binary = pack("H*", $hex);
-        return new static($binary, $byteSize);
+        return isset($this->data[$offset]);
     }
 
     /**
-     * @param int      $start
-     * @param integer|null $end
-     * @return BufferInterface
-     * @throws \Exception
+     * offsetUnset
+     *
+     * @param mixed $offset
+     * @return void
      */
-    public function slice(int $start, int $end = null): BufferInterface
+    public function offsetUnset($offset): void
     {
-        if ($start > $this->getSize()) {
-            throw new \Exception('Start exceeds buffer length');
-        }
+        unset($this->data[$offset]);
+    }
 
+    /**
+     * offsetGet
+     *
+     * @param mixed $offset
+     * @return mixed
+     */
+    public function offsetGet($offset): mixed
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+
+    /**
+     * toString
+     *
+     * @param string $encoding
+     * @return string
+     */
+    public function toString(string $encoding='utf8')
+    {
+        $output = '';
+        $input = $this->data;
+
+        switch ($encoding) {
+            case 'hex':
+            foreach ($input as $data)  {
+                $hex = dechex($data);
+
+                // pad zero
+                if ((strlen($hex) % 2) !== 0) {
+                    $hex = '0' . $hex;
+                }
+                $output .= $hex;
+            }
+            break;
+            case 'ascii':
+            foreach ($input as $data)  {
+                $output .= chr($data);
+            }
+            break;
+            case 'utf8':
+            // $length = count($input);
+
+            // for ($i = array_keys($input)[0]; $i < $length; $i += 3) {
+            //     $output .= chr($input[$i]) . chr($input[$i + 1]) . chr($input[$i + 2]);
+            // }
+            // change to bytes string
+            foreach ($input as $data)  {
+                $output .= chr($data);
+            }
+            break;
+            default:
+            throw new InvalidArgumentException('ToString encoding must be valid.');
+            break;
+        }
+        return $output;
+    }
+
+    /**
+     * length
+     *
+     * @return int
+     */
+    public function length()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * concat
+     *
+     * @param mixed $inputs
+     * @return \RLP\Buffer
+     */
+    public function concat()
+    {
+        $inputs = func_get_args();
+
+        foreach ($inputs as $input) {
+            if (is_array($input)) {
+                $input = new Buffer($input);
+            }
+            if ($input instanceof Buffer) {
+                $length = $input->length();
+
+                for ($i = 0; $i < $length; $i++) {
+                    $this->data[] = $input[$i];
+                }
+            } else {
+                throw new InvalidArgumentException('Input must be array or Buffer when call concat.');
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * slice
+     *
+     * @param int $start
+     * @param mixed $end
+     * @return \RLP\Buffer
+     */
+    public function slice(int $start=0, $end=null)
+    {
         if ($end === null) {
-            return new static(substr($this->getBinary(), $start));
+            $end = $this->length();
         }
-
-        if ($end > $this->getSize()) {
-            throw new \Exception('Length exceeds buffer length');
+        if ($end > 0) {
+            $end -= $start;
+        } elseif ($end === 0) {
+            return new Buffer([]);
         }
+        $sliced = array_slice($this->data, $start, $end);
+        return new Buffer($sliced);
+    }
 
-        $string = substr($this->getBinary(), $start, $end);
-        if (!is_string($string)) {
-            throw new \RuntimeException('Failed to slice string of with requested start/end');
+    /**
+     * decodeToData
+     *
+     * @param mixed $input
+     * @return array
+     */
+    protected function decodeToData($input)
+    {
+        $output = [];
+
+        if (is_array($input)) {
+            $output = $this->arrayToData($input);
+        } elseif (is_int($input)) {
+            $output = $this->intToData($input);
+        } elseif (is_numeric($input)) {
+            $output = $this->numericToData($input);
+        } elseif (is_string($input)) {
+            $output = $this->stringToData($input, $this->encoding);
         }
+        return $output;
+    }
 
-        $length = strlen($string);
-        return new static($string, $length);
+    /**
+     * arrayToData
+     *
+     * @param array $inputs
+     * @return array
+     */
+    protected function arrayToData(array $inputs)
+    {
+        $output = [];
+
+        foreach ($inputs as $input) {
+            if (is_array($input)) {
+                // throw exception, maybe support future
+                // $output[] = $this->arrayToData($input);
+                throw new InvalidArgumentException('Do not use multidimensional array.');
+            } elseif (is_string($input)) {
+                $output = array_merge($output, $this->stringToData($input, $this->encoding));
+            } elseif (is_numeric($input)) {
+                $output = array_merge($output, $this->numericToData($input));
+            }
+        }
+        return $output;
+    }
+
+    /**
+     * stringToData
+     *
+     * @param string $input
+     * @param string $encoding
+     * @return array
+     */
+    protected function stringToData(string $input, string $encoding)
+    {
+        $output = [];
+
+        switch ($encoding) {
+            case 'hex':
+            if (strpos($input, '0x') === 0) {
+                // hex string
+                $input = str_replace('0x', '', $input);
+            }
+            if (strlen($input) % 2 !== 0) {
+                $input = '0' . $input;
+            }
+            // $splited = str_split($input, 2);
+
+            // foreach ($splited as $data)  {
+            //     $output[] = hexdec($data);
+            // }
+            $output = array_map('hexdec', str_split($input, 2));
+
+            break;
+            case 'ascii':
+            $output = array_map('ord', str_split($input, 1));
+            break;
+            case 'utf8':
+            $output = unpack('C*', $input);
+            break;
+            default:
+            throw new InvalidArgumentException('StringToData encoding must be valid.');
+            break;
+        }
+        return $output;
+    }
+
+    /**
+     * numericToData
+     *
+     * @param mixed $intput
+     * @return array
+     */
+    protected function numericToData($intput)
+    {
+        $output = (int) $intput;
+
+        return [$output];
+    }
+
+    /**
+     * intToData
+     *
+     * @param mixed $intput
+     * @return array
+     */
+    protected function intToData($input)
+    {
+        return array_fill(0, $input, 0);
     }
 }
